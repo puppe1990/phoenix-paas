b166b77829700e6fb7a74be50f54e979fa6ebac7
puppe1990 <matheus.puppe90@hotmail.com>
2026-06-28 20:59:46 -0300

    test(dashboard): cover authenticated dashboard navigation

--- test/phoenix_paas_web/live/dashboard_live_test.exs
+++ test/phoenix_paas_web/live/dashboard_live_test.exs
@@ -4,11 +4,45 @@ defmodule PhoenixPaasWeb.DashboardLiveTest do
   import Phoenix.LiveViewTest
 
   alias PhoenixPaas.TenancyFixtures
+  alias PhoenixPaasWeb.UserAuth
 
   setup :register_and_log_in_user
 
   test "redirects when not logged in" do
     assert {:error, {:redirect, %{to: "/users/log-in"}}} = live(build_conn(), ~p"/")
+  end
+
+  test "renders dashboard when only remember-me cookie is present", %{user: user} do
+    logged_in_conn =
+      build_conn()
+      |> Map.replace!(:secret_key_base, PhoenixPaasWeb.Endpoint.config(:secret_key_base))
+      |> init_test_session(%{})
+      |> fetch_cookies()
+      |> UserAuth.log_in_user(user, %{"remember_me" => "true"})
+
+    %{value: signed_token} = logged_in_conn.resp_cookies["_phoenix_paas_web_user_remember_me"]
+
+    conn =
+      logged_in_conn
+      |> recycle()
+      |> Map.replace!(:secret_key_base, PhoenixPaasWeb.Endpoint.config(:secret_key_base))
+      |> put_req_cookie("_phoenix_paas_web_user_remember_me", signed_token)
+      |> init_test_session(%{user_remember_me: true})
+
+    refute get_session(conn, :user_token)
+
+    {:ok, view, html} = live(conn, ~p"/")
+    assert has_element?(view, "#dashboard")
+    assert html =~ "Phoenix PaaS"
+  end
+
+  test "renders dashboard when only user_id is present in session", %{user: user} do
+    conn =
+      build_conn()
+      |> init_test_session(%{user_id: user.id})
+
+    {:ok, view, _html} = live(conn, ~p"/")
+    assert has_element?(view, "#dashboard")
   end
 
   test "renders dashboard overview for tenant", %{conn: conn, scope: scope} do
